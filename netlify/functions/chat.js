ce1b87a166169393d66fa0e8d96e929fa1a11a42
arnoldsala7-lgtm <arnoldsala7@gmail.com>
2026-02-27 23:48:24 -0500

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -1,39 +1,8 @@
-exports.handler = async function(event) {
-
-if (event.httpMethod !== "POST")
-return { statusCode:405, body:"Método no permitido" };
+async function enviarMensaje(mensaje){
 
 try{
 
-const apiKey = process.env.GEMINI_API_KEY;
-
-if(!apiKey){
-
-return{
-statusCode:200,
-body:JSON.stringify({
-respuesta:"⚠️ Falta API KEY"
-})
-};
-
-}
-
-const { prompt , historial } = JSON.parse(event.body);
-
-const mensajes = historial || [];
-
-mensajes.push({
-
-role:"user",
-parts:[{text:prompt}]
-
-});
-
-const response = await fetch(
-
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
-
-{
+const res = await fetch("/.netlify/functions/chat",{
 
 method:"POST",
 
@@ -41,73 +10,27 @@ headers:{
 "Content-Type":"application/json"
 },
 
-body:JSON.stringify({
+body: JSON.stringify({
 
-systemInstruction:{
-parts:[{
-text:"Eres el asistente de ElCerveceroTV creado por Arnold."
-}]
-},
-
-contents:mensajes
+prompt: mensaje,
+historial: []
 
 })
 
-}
+});
 
-);
+const data = await res.json();
 
-const data = await response.json();
+console.log(data); // mira consola
 
-
-// 👇 SI GOOGLE BLOQUEA
-if(data.error){
-
-return{
-
-statusCode:200,
-
-body:JSON.stringify({
-
-respuesta:"⏳ Estoy ocupado 😅 intenta otra vez en unos segundos."
-
-})
-
-};
-
-}
-
-
-return{
-
-statusCode:200,
-
-body:JSON.stringify({
-
-respuesta:
-
-data.candidates?.[0]?.content?.parts?.[0]?.text
-
-|| "Sin respuesta"
-
-})
-
-};
+return data.respuesta;
 
 }catch(error){
 
-return{
+console.error(error);
 
-statusCode:200,
-
-body:JSON.stringify({
-
-respuesta:"⚠️ "+error.message
-
-})
-
-};
+return "Error de conexión con el servidor. Intenta de nuevo. 🍺";
 
 }
 
-};
+}
